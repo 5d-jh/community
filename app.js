4f2c3d19900d6360b618110a86889859d423aaef
5d-jh <jeonghyun0126@outlook.com>
2019-01-20 06:40:41 +0900

    CORS 지원

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost/community', {useNewUrlParser: true})
 
 const app = express();
 
 app.listen(8080);
+app.use(cors());
 app.use(session({
     secret: '!@#%^FM',
     resave: false,
